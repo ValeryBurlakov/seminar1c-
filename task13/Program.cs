31d0c847d7cadab1fb048148ea1198cee6b32d3d
Valery <wogl@mail.ru>
2022-11-15 21:29:53 +0300

    Sem2 c# task 9 11 13 15

---
+++ task13/Program.cs
@@ -0,0 +1,38 @@
+﻿// // Напишите программу, которая принимает на вход 
+// число и проверяет, кратно ли оно одновременно 
+// // 7 и 23.
+
+// // 14 -> нет 
+// // 46 -> нет 
+// // 161 -> да
+// // 8,9  ->  нет
+
+Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int mod = number1 % number2;
+int mod2 = number1 % number3;
+if(mod == 0 && mod2 == 0)
+{
+    Console.WriteLine("Число кратно");
+}
+else
+{
+    Console.WriteLine("Число не кратно");
+}
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// if ((number%7 == 0) && (number%23 == 0))
+// {
+//     Console.WriteLine("Число кратно и 7 и 23");
+// }
+// else
+// {
+//     Console.WriteLine("Число не кратно");
+// }
+
---
+++ task15/Program.cs
@@ -0,0 +1,43 @@
+﻿// // Напишите программу, которая принимает на вход 
+// два числа и проверяет, является ли одно число 
+// квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+// Console.WriteLine("Введите первое число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+
+// if(number1*number1 == number2 || numberb * numberb) == number1)
+// {
+//     Console.WriteLine("является квадратом");
+// }
+// else
+// {
+// Console.WriteLine("Не является квадратом");
+// }
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+// 5, 25  ->  да
+// -4, 16  ->  да
+// 25, 5  ->  да
+// 8,9  ->  нет
+
+System.Console.Write("Введите первое число\t");
+int numberA= Convert.ToInt32 (Console.ReadLine());
+
+System.Console.Write("Введите Второе число\t");
+int numberB= Convert.ToInt32 (Console.ReadLine());
+
+if (numberA*numberA==numberB || numberB*numberB==numberA){
+System.Console.Write($"да");
+}
+else{
+    System.Console.Write($"нет");
+}
+
