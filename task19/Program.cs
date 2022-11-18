3ddd8f692f42f63af5101054b0b881cf5ea73506
Valery <wogl@mail.ru>
2022-11-18 21:28:06 +0300

    task 17 19 21 23

---
+++ task19/Program.cs
@@ -0,0 +1,65 @@
+﻿// по заданному номеру четверти , показывает диапазон возможных координат
+Console.WriteLine("Введите номер четверти");
+int number = Convert.ToInt32(Console.ReadLine());
+// if(number >= 1 && number <= 4)
+if (number == 1)
+{
+    Console.WriteLine("x > 0, y > 0");
+}
+else if (number == 2)
+{
+    Console.WriteLine("x < 0, y > 0");
+}
+else if (number == 3)
+{
+    Console.WriteLine("x < 0, y < 0");
+}
+else if (number == 4)
+{
+    Console.WriteLine("x > 0, y < 0");
+}
+// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+// string[] arr={"x>0 и y>0","x<0 и y>0","x<0 и y<0","x>0 и y<0"};
+// System.Console.WriteLine("Введите номер четверти");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+// if(num==1){
+// System.Console.WriteLine($"Номер четверти{arr[num-1]}");
+// }
+// else if(num==2){
+// System.Console.WriteLine($"Номер четверти{arr[num-1]}");
+// }
+// else if(num==3){
+// System.Console.WriteLine($"Номер четверти{arr[num-1]}");
+// }
+// else if(num==4){
+// System.Console.WriteLine($"Номер четверти{arr[num-1]}");
+// }
+
+
+
+// Console.Write("Введите номер четверти -> ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string[] list = {"x > 0 and y > 0", "x < 0 and y > 0", "x < 0 and y < 0", "x > 0 and y < 0"};
+
+// Console.WriteLine(list[num-1])
+
+
+
+
+// string [] quarters = {"x > 0; y > 0",// 0
+//                       "x < 0; y > 0",// 1
+//                       "x < 0; y < 0",// 2
+//                       "x > 0; y < 0"// 3
+// };
+// Console.WriteLine("Введите номер четверти: ");
+// int quarter = Convert.ToInt32(Console.ReadLine());
+// if(quarter >= 1 && quarter <= 4)
+// {
+//     Console.WriteLine(quarters[quarter-1]);
+// }
+// else
+// {
+//     Console.WriteLine("Такой четверти нет");
+// }
---
+++ task21/Program.cs
@@ -0,0 +1,49 @@
+﻿// принимае на вход координаты двух точек и находит расстояние между ними в 2д пространстве
+// A (3,6)
+//Math.Sqrt()
+//Math.Pow(a, 2)
+
+// Double x1 = 0;
+// Double x2 = 0;
+// Double y1 = 0;
+// Double y2 = 0;
+// Double result;
+// Console.WriteLine("Введите x1:");
+// x1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите x2:");
+// x2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите y1:");
+// y1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите y2:");
+// y2 = Convert.ToDouble(Console.ReadLine());
+
+// result = Math.Sqrt(Math.Pow(x2-x1, 2) + Math.Pow(y2-y1, 2));
+// Console.WriteLine($"отрезок равен:  {result}");
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09 
+// A (7,-5); B (1,-1) -> 7,21
+
+System.Console.WriteLine("Введите x первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите y первой точки");
+int y1 = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите x второй точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.WriteLine("Введите y второй точки");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+int TotalX = x1-x2;
+
+int TotalY = y1-y2;
+
+double lenght = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+// double lenght = Math.Round( Math.Sqrt (Math.Pow(TotalX,2) + Math.Pow(TotalY,2)),2); идет результат с округлением
+System.Console.WriteLine(lenght);
+
+// |AB|² = (y2 - y1)² + (x2 - x1)² как я понял её надо
+// double result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
