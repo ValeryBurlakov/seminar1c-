0da754418f1d0b48910ab0a3335a90042e7cc7d5
Valery <wogl@mail.ru>
2022-11-25 22:22:28 +0300

    31 35 37

---
+++ task35/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 32: Напишите программу 
+// замена элементов массива: 
+// положительные элементы замените 
+// на соответствующие отрицательные, 
+// и наоборот.
+// [-4, -8, 8, 2] -> [4, 8, -8, -2
+
+// Console.WriteLine("введите число, которое хотите найти: ");
+// int number = Convert.ToInt32(Console.ReadLine);
+
+// int[] array = new int[8];
+// for (int i = 0; i < array.Length; i++)
+// {
+//     array[i] = new Random().Next(10, 99);
+// }
+// Console.Write('[' + string.Join(", ", array) + "]");
+// for (int j = 0; j < array.Length; j++)
+// {
+//     if (array[j] > 0)
+//         array[j] = array[j] * -1;
+//     else
+//     {
+//         array[j] = array[j] * -1;
+//     }
+// }
+
+// Console.Write('[' + string.Join(", ", array) + "]");
+
+
+// Задача 33: Задайте массив. 
+// Напишите программу, которая определяет,
+//  присутствует ли заданное число 
+//  в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+int[] FillArray(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9, 10);
+    } 
+    return arr;
+}
+
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+
+Console.WriteLine(string.Join(", ", array));
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+bool isOk = false;
+for (int i = 0; i < array.Length;i++)
+{
+    if (number == array[i])
+    {
+        isOk = true;
+        break;
+    }
+}
+if(isOk)
+{
+    Console.WriteLine("элемент найден");
+}
+else{
+    Console.WriteLine("не найден");
+}
+// Можно еще одну переменную добавить, в которую будет писаться количество раз, которое заданное число встретилось
---
+++ task37/Program.cs
@@ -0,0 +1,126 @@
+﻿// Задача 35: Задайте одномерный массив из 123 случайных чисел. Найдите количество элементов массива, значения которых лежат в отрезке [10,99]. 
+// Пример для массива из 5, а не 123 элементов. В своём решении сделайте для 123
+// [5, 18, 123, 6, 2] -> 1
+// [1, 2, 3, 6, 2] -> 0
+// [10, 11, 12, 13, 14] -> 5
+
+// int[] FillArray(int size)
+// {
+//     int[] arr = new int[size];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(0, 100);
+//     } 
+//     return arr;
+// }
+
+// Console.Write("Введите длину массива: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int[] array = FillArray(num);
+// Console.WriteLine(string.Join(", ", array));
+// // int minposition = 9;
+// // int maxposition = 100;
+// int count = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//     if (array[i] >= 10 && array[i] <= 99)
+//     {
+//         count++;
+//     i++;
+//     }
+// }
+// Console.WriteLine(count);
+
+
+// Задача 37: Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+// int[] FillArray(int size)
+// {
+//     int[] arr = new int[size];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(0, 100);
+//     } 
+//     return arr;
+// }
+
+// Console.Write("Введите длину массива: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int[] array = FillArray(num);
+// Console.WriteLine(string.Join(", ", array));
+// int j = array.Length - 1;
+// int[] sumarr = new int [(j - j/2)];
+// for (int i = 0; i < sumarr.Length; i++, j--)
+// {
+//     if (i == j)
+//     {
+//         sumarr[i] = array[i];
+//         break;
+//     }
+
+//     sumarr[i] = array[i] * array[j];
+    
+// }
+// Console.WriteLine(string.Join(" ", sumarr));
+
+
+
+// int[] FillArray(int size)
+// {
+//     int[] arr = new int[size];
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         arr[i] = new Random().Next(1, 10);
+//     }
+//     return arr;
+// }
+// Console.Write("Введите длину массива: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int[] arr = FillArray(num);
+// int num1 = num / 2 + num % 2;
+// int[] NewArr = new int[num1];
+// Console.WriteLine(string.Join(", ", arr));
+
+// if (num % 2 != 0)
+// {
+//     for (int i = 0; i < arr.Length / 2; i++)
+//         NewArr[i] = arr[i] * arr[arr.Length - 1 - i];
+//     NewArr[num1 - 1] = arr[num1 - 1];
+// }
+// else
+//     for (int i = 0; i < arr.Length / 2; i++)
+//         NewArr[i] = arr[i] * arr[arr.Length - 1 - i];
+
+// Console.WriteLine(string.Join(", ", NewArr));
+
+
+
+int[] FillArray(int size)
+{
+    
+    int[] arr = new int[size];
+    for(int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-9,10);
+    }
+    return arr;
+}
+
+Console.Write("Введите длину массива: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArray(num);
+Console.WriteLine(string.Join(", ", array));
+
+int newLength = array.Length % 2 + array.Length/2;
+int[] newArray = new int[newLength];
+for(int i = 0; i < array.Length/2; i++)
+{
+    newArray[i] = array[i] * array[array.Length-1-i];
+}
+if (array.Length % 2 > 0)
+{
+    newArray[newArray.Length-1] = array[array.Length/2];
+}
+Console.WriteLine(string.Join(", ", newArray));
